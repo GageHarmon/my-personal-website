368e48f34e8da8201fe458e2323a1b8be76c93a2
Gage Harmon <gaharmon1@gmail.com>
2023-02-06 08:25:34 -0700

    messing with a lot of sidebar/toggle options

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+let menuToggle = document.querySelector('.menuToggle');
+let navigation = document.querySelector('.navigation');
 
-document.querySelector("body").appendChild(h2);
+menuToggle.onclick = function () {
+    navigation.classList.toggle('open');
+}
+
+let list = document.querySelectorAll('.list');
+function activeLink() {
+    list.forEach((item) =>
+        item.classList.remove('active'));
+    this.classList.add('active');
+}
+list.forEach((item) =>
+    item.addEventListener('click', activeLink))
+
+
+// const h2 = document.createElement("h2");
+// h2.textContent = "This content added by JavaScript";
+
+// document.querySelector("body").appendChild(h2);
